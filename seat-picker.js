14447c534f9c109a5bcb3878cdca4fbc4f51f2c5
Lukas Zanner <zannix@freenet.de>
2019-05-15 00:03:15 +0200

    Add bootstrap and make it prettier

--- seat-picker.js
+++ seat-picker.js
@@ -1,5 +1,5 @@
 class SeatPicker {
-    EMPTY = -1;
+    EMPTY = "-";
     seats = [];
     uniqueGroupId = -1;
     numberOfGuests = -1;
@@ -11,29 +11,36 @@ class SeatPicker {
         }
         this.uniqueGroupId = -1;
         this.numberOfGuests = 0;
-
-        console.log(this);
     }
 
     addGuests(groupSize) {
+        $("#formNewGuestsFeedback").text("");
+
+        if (groupSize == "") {
+            return;
+        } else if (isNaN(groupSize)) {
+            $("#formNewGuestsFeedback").text("Only numbers are allowed.");
+            return;
+        }
+
         // Check the boundaries
         if (groupSize <= 0) {
-            console.log("Only positive numbers are allowed. (addGuests)");
+            $("#formNewGuestsFeedback").text("Only positive numbers are allowed.");
             return;
         } else if (groupSize > this.seats.length) {
-            console.log("The group is too big for this restaurant. (addGuests)");
+            $("#formNewGuestsFeedback").text("The group is too big for this restaurant.");
             return;
         }
 
         if(groupSize > this.seats.length - this.numberOfGuests) {
-            console.log("Not enough free seats available. (addGuests)");
+            $("#formNewGuestsFeedback").text("Not enough free seats available.");
             return;
         }
 
         // Look for free seats available for this group and get the indices of the most suitable ones in a row
         var seatNumbers = this.findBestFreeSeats(groupSize);
         if (seatNumbers == null) {
-            console.log("No findBestFreeSeats seat numbers were found. (addGuests)");
+            $("#formNewGuestsFeedback").text("No suitable seats were found.");
             return;
         }
 
@@ -44,22 +51,31 @@ class SeatPicker {
         }
         this.numberOfGuests += parseInt(groupSize, 10);
 
-        console.log(this);
+        $("#guestTable").text(sp.seats);
     }
 
     removeGuestGroup(uniqueGroupId) {
+        $("#formLeavingGuestsFeedback").text("");
+
+        if (uniqueGroupId == "") {
+            return;
+        } else if (isNaN(uniqueGroupId)) {
+            $("#formLeavingGuestsFeedback").text("Only numbers are allowed.");
+            return;
+        }
+
         // Check boundaries
         if (uniqueGroupId < 0) {
-            console.log("Group Ids need to be >= 0. (removeGuestGroup)");
+            $("#formLeavingGuestsFeedback").text("The group id needs to be greater or equal to 0.");
             return;
         } else if (uniqueGroupId > this.uniqueGroupId) {
-            console.log("No group with id " + uniqueGroupId + " found. (removeGuestGroup)");
+            $("#formLeavingGuestsFeedback").text("No group with id " + uniqueGroupId + " found.");
             return;
         }
 
         // Check if the supplied group exists
         if (this.seats.findIndex(element => element == uniqueGroupId) == -1) {
-            console.log("No group with id " + uniqueGroupId + " found. (removeGuestGroup)");
+            $("#formLeavingGuestsFeedback").text("No group with id " + uniqueGroupId + " is currently at the restaurant.");
             return;
         }
 
@@ -71,13 +87,13 @@ class SeatPicker {
             }
         }
 
-        console.log(this);
+        $("#guestTable").text(sp.seats);
     }
 
     findBestFreeSeats(groupSize) {
         var availableSeats = [];
     
-        // If the seat array is completely empty, an array with all seats is returned
+        // All seats are empty, so return all of them
         if (this.seats.findIndex(element => element != this.EMPTY) == -1) {
             for (var i = 0; i < this.seats.length; ++i) {
                 availableSeats.push(i);
@@ -85,11 +101,10 @@ class SeatPicker {
             return availableSeats;
         }
 
-        // Get an array of arrays of indices of free seats in a row. A seat row with more free seats
-        // are further at the back of the array
+        // Get an array of arrays of indices of free seats in a row.
+        // Rows with more free seats in a row are further at the back of the array.
         availableSeats = this.findAllFreeSeatsInARow();
         if (availableSeats == null) {
-            console.log("Not enough seats in a row available! (findBestFreeSeats)");
             return null;
         }
 
@@ -101,7 +116,6 @@ class SeatPicker {
             }
         }
         if (availableSeats.length == 0) {
-            console.log("Not enough free seats in a row available for this group! (findBestFreeSeats)");
             return null;
         }
 
@@ -112,12 +126,17 @@ class SeatPicker {
         var availableSeats = [];
         var nextSeatIndex = 0;
 
+        // At least one free seat should be available
+        if (this.seats.findIndex(element => element == this.EMPTY) == -1) {
+            return null;
+        }
+
         // Go to the start index of an empty row of seats
         if (this.seats[nextSeatIndex] == this.EMPTY) {
             do {
                 nextSeatIndex = this.decrementSeatIndex(nextSeatIndex);
             } while (this.seats[nextSeatIndex] == this.EMPTY);
-            // The index is on the last taken seat, so we have to increment it by one
+            // The index is now on the last taken seat, so we have to increment it by one
             nextSeatIndex = this.incrementSeatIndex(nextSeatIndex);
         } else {
             do {
@@ -158,40 +177,42 @@ class SeatPicker {
         var pickedRowIndex = -1;
         var isPickLeftAligned = true;
 
-        // Pick the seat row that is sizewise closest to the group and that is closest to the highest group id
-        var highestGroupId = -1;
-        var lowestSeatRowLength = availableSeats[0].length;
-        for (var i = 0; i < availableSeats.length; ++i) {
-            if (availableSeats[i].length != lowestSeatRowLength) {
-                break;
-            }
-            
-            var currentRow = availableSeats[i];
+        {
+            // Pick the seat row that is sizewise closest to the group and that is closest to the highest group id
+            var highestGroupId = -1;
+            var lowestSeatRowLength = availableSeats[0].length;
+            for (var i = 0; i < availableSeats.length; ++i) {
+                // Only look at seat rows that are the closest to the group's size
+                if (availableSeats[i].length != lowestSeatRowLength) {
+                    break;
+                }
 
-            // Determine the group id on both sides of the current seat row
-            var leftGroupId = this.seats[this.decrementSeatIndex(currentRow[0])];
-            var rightGroupId = this.seats[this.incrementSeatIndex(currentRow[currentRow.length - 1])];
-            var currentHighestGroupId = -1;
+                // Determine the group id on both sides of the current seat row
+                var currentRow = availableSeats[i];
+                var leftGroupId = this.seats[this.decrementSeatIndex(currentRow[0])];
+                var rightGroupId = this.seats[this.incrementSeatIndex(currentRow[currentRow.length - 1])];
 
-            // Pick the group id that is higher so the group is placed next to another group that will still spend
-            // about the same amount of time in the restaurant. Groups that are still in the restaurant are therefore
-            // more or less tightly packed together and give more room to bigger groups possibly coming in the future.
-            if (rightGroupId > leftGroupId) {
-                currentHighestGroupId = rightGroupId;
-                isPickLeftAligned = false;
-            } else {
-                currentHighestGroupId = leftGroupId;
-                isPickLeftAligned = true;
-            }
+                // Pick the group id that is higher so the group is placed next to another group that will still spend
+                // about the same amount of time in the restaurant. Groups that are still in the restaurant are therefore
+                // more or less tightly packed together and give more room to bigger groups possibly coming in the future.
+                var currentHighestGroupId = -1;
+                if (rightGroupId > leftGroupId) {
+                    currentHighestGroupId = rightGroupId;
+                    isPickLeftAligned = false;
+                } else {
+                    currentHighestGroupId = leftGroupId;
+                    isPickLeftAligned = true;
+                }
 
-            if (currentHighestGroupId > highestGroupId) {
-                highestGroupId = currentHighestGroupId;
-                pickedRowIndex = i;
+                if (currentHighestGroupId > highestGroupId) {
+                    highestGroupId = currentHighestGroupId;
+                    pickedRowIndex = i;
+                }
             }
         }
 
         // Depending on which side the group should be placed closer to, the seat indices are cut so that
-        // only the seats that the group should be placed at are left
+        // only the seats that the group should be placed at are left in the array.
         var deleteCount = availableSeats[pickedRowIndex].length - groupSize;
         if (!isPickLeftAligned) {
             availableSeats[pickedRowIndex].splice(0, deleteCount);
